e1b6e595a3aed9321d76c00ce5bb3c508e404c68
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-10-10 11:02:22 +0200

    explicación de liista terminada

---
+++ LIstas.py
@@ -0,0 +1,25 @@
+#Listas en opython
+#Definición e inicialización
+vNOMBRES = []
+
+#Insertar un dato
+vNOMBRES.insert(0,"Juan")
+vNOMBRES.insert(1,"Pepe")
+vNOMBRES.insert(2,"Inés")
+vNOMBRES.append("Minerva")
+vNOMBRES.insert(1,"Rufino")
+
+##Eliminar un elemento
+    #vNOMBRES.clear()
+vNOMBRES.remove("Pepe")
+print("EL nombre borrado es", vNOMBRES.pop(1))
+
+#Ordenar una lista
+#Con reverse puedo ordenar en inverso
+vNOMBRES.sort(reverse=True)
+
+#Contar en el números de elementos de la lista
+print("Minerva aparece: ",vNOMBRES.count("Minerva"),"veces")
+print("La lista tiene", len(vNOMBRES),"nombres")
+
+print(vNOMBRES)
