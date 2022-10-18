c18a967d5fae0ea5147461fc9c2367e72c3faa81
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-10-18 14:31:39 +0200

    explicaciones:)

--- Funciones.py
+++ Funciones.py
@@ -1,3 +1,4 @@
+'''
 edad = 5
 
 edad_como_texto = str(edad)
@@ -8,3 +9,19 @@ print(edad/5)
 print(len("Juan"))
 
 edad2 = int(input("Dime tu edad:"))
+'''
+
+#Funciones definidas por el usuario
+
+def imprimirHola(nombre:str, apellido:str):
+    print("Hola ",nombre,apellido)
+
+imprimirHola("Juan", "Zamora")
+
+
+def imprimirSuma(num1:int, num2:int):
+    #print("La suma de", num1, "+", num2,"=", num1+num2)
+    return num1+num2
+
+suma=imprimirSuma(2,3)
+print("La suma es de: ", suma)
