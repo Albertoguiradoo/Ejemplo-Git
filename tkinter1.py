f5bc38d084e0c6fcbc3d307172fc1aba4f3d65b8
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-11-21 11:14:40 +0100

    set y get

--- tkinter1.py
+++ tkinter1.py
@@ -1,22 +1,40 @@
 from tkinter import *
 from tkinter import ttk
+
+def saludar():
+    texto = campoTexto.get()
+    textoLabel.set(texto)
+
+
 #Generar la ventana
 ventana = Tk()
-ventana.config(background="aquamarine")
+ventana.config(background="red")
 ventana.title("Que pasa primo!")
-ventana.geometry("500x500")
+ventana.geometry("1000x1000")
 ventana.resizable(width=False,height=False)
+
 imagen=PhotoImage(file="Espana.gif")
-fondo=Label(ventana,image=imagen).place(x=0,y=0)
+fondo=Label(ventana,image=imagen).place(x=100,y=150)
+
+imagen2=PhotoImage(file="escudo.png")
+fondo=Label(ventana,image=imagen2).place(x=550,y=150)
+
 
 #Genera el liebnzo para pintar componentes
 frm = ttk.Frame(ventana).pack()
 
 #Componentes Label y Button
-labelTexto = ttk.Label(frm, text="Viva España!")
-labelTexto.config(background="light blue",border=5,font=("Arial",15))
+textoLabel = StringVar()
+textoLabel.set("Viva España!")
+labelTexto = ttk.Label(frm, textvariable=textoLabel)
+labelTexto.config(background="yellow",border=5,font=("Arial",55))
 labelTexto.pack()
-ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
 
+#Componente cuadro de texto 
+campoTexto=ttk.Entry(frm)
+campoTexto.pack()
+
+ttk.Button(frm, text="Saludos", command=saludar).pack()
+ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
 
 ventana.mainloop()
