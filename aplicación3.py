096faa7a37f9e482e500b289c2c6df132ee65488
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-12-01 10:03:15 +0100

    nos hemos quedado en el .set

--- aplicación3.py
+++ aplicación3.py
@@ -1,20 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+import tkinter as tk
 
 usuario= ""
 contraseña= ""
 contraseña2= ""
+genero=""
 vNum= []
 def guardarDAtos():
+    genero=combo_género .get()
     usuario = entry_nombre_usuario.get()
     contraseña= entry_contraseña.get()
     contraseña2=entry_contraseña_repetir.get()
     if contraseña == contraseña2:
-        print(f"Usuario: {usuario}\ncontraseña: {contraseña2}")
+        print(f"Usuario: {usuario}\ncontraseña: {contraseña2}\ngénero:{genero}")
         vNum.append(usuario)
         vNum.append(contraseña)
+        vNum.append(genero)
         messagebox.showinfo("Usuario guardado",f"Usuario {usuario} guardado :)")
+    
     else:
         print("Las contraseñas no coinciden, por favor intentelo de nuevo")
     
@@ -22,7 +27,7 @@ def guardarDAtos():
 
 ventana = Tk()
 ventana.config(background= "light blue")
-ventana.geometry("400x200")
+ventana.geometry("500x300")
 ventana.resizable(width=FALSE, height=FALSE)
 
 #Titulo de la aplicación
@@ -44,6 +49,8 @@ entry_contraseña= ttk.Entry(ventana, show=" ")
 label_contraseña_repetir= ttk.Label(ventana, text="Confirma la contraseña: ")
 entry_contraseña_repetir= ttk.Entry(ventana,show=" ")
 
+label_género= ttk.Label(ventana, text="Género:")
+
 botón_guardar= ttk.Button(ventana, text="Guardar", command=guardarDAtos)
 botón_salir= ttk.Button(ventana, text= "Salir", command=ventana.destroy)
 
@@ -59,6 +66,16 @@ entry_contraseña.grid(row=2, column=1, pady= 5)
 label_contraseña_repetir.grid(row=3, column=0, pady= 5,padx=15)
 entry_contraseña_repetir.grid(row=3, column=1, pady= 5)
 
-botón_guardar.grid(row=4, column=0, pady= 5)
-botón_salir.grid(row=4, column=1, pady= 5)
+botón_guardar.grid(row=5, column=0, pady= 6)
+botón_salir.grid(row=5, column=1, pady= 6)
+
+label_género.grid(row=4, column=0, pady= 5)
+
+#Combobox   
+combo_género = ttk.Combobox(ventana, values=["Mujer", "Hombre", "Género no binario", "No espeficicar"])
+combo_género .grid(row=4, column=1, pady= 5)
+combo_género.set("Espeficique su género")
+
+
+
 ventana.mainloop()
