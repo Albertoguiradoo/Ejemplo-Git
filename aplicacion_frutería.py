39d230cab26a0764d43f11c65c4e82440a9de858
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2023-01-09 11:11:38 +0100

    hay q arreglar lo de sumar y restar

--- aplicacion_frutería.py
+++ aplicacion_frutería.py
@@ -3,7 +3,29 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title="Bienvenido a su frutería de confianza!"
-    
+    caja_número_1= ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    caja_número_2= ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    caja_número_3= ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    caja_número_4= ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    caja_número_5= ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    def minus_click(e):
+        caja_número_1.value = str(int(caja_número_1.value) - 1)
+        caja_número_2.value = str(int(caja_número_2.value) - 1)
+        caja_número_3.value = str(int(caja_número_3.value) - 1)
+        caja_número_4.value = str(int(caja_número_4.value) - 1)
+        caja_número_5.value = str(int(caja_número_5.value) - 1)
+        
+        page.update()
+
+    def plus_click(e):
+        caja_número_1.value = str(int(caja_número_1.value) + 1)
+        caja_número_2.value = str(int(caja_número_2.value) + 1)
+        caja_número_3.value = str(int(caja_número_3.value) + 1)
+        caja_número_4.value = str(int(caja_número_1.value) + 1)
+        caja_número_5.value = str(int(caja_número_5.value) + 1)
+        
+        page.update()
+
     def finalizar_Compra():
         print(vCompra)
 
@@ -20,10 +42,11 @@ def main(page: ft.Page):
         else:
             print("No ha seleccionado nada.")
         
+   
+    
     botón_Añadir_Verduras=ft.FilledButton(text="Añadir", icon="Añadir", on_click=añadir_producto)
     botón_Añadir_Carnes=ft.FilledButton(text="Añadir", icon="Añadir", on_click=añadir_producto)
     botón_Añadir_Pescados=ft.FilledButton(text="Añadir", icon="Añadir", on_click=añadir_producto)
-    botón_Añadir_Sales=ft.FilledButton(text="Añadir", icon="Añadir", on_click=añadir_producto)
     botón_Añadir_Cervezas=ft.FilledButton(text="Añadir", icon="Añadir", on_click=añadir_producto)
     botón_Añadir_Botellas=ft.FilledButton(text="Añadir", icon="Añadir", on_click=añadir_producto)
     botón_Finalizar_Compra=ft.FilledButton(text="Finalizar Compra", icon="Añadir", on_click=finalizar_Compra)
@@ -46,9 +69,13 @@ def main(page: ft.Page):
                                                     ft.dropdown.Option("tomate para el mejor gazpacho 2$/kg")])
     fila1 = ft.Row(spacing=50, controls=[dropDownMenúVerduras,botón_Añadir_Verduras] )                                             
     page.add(fila1)
-    slider_Kilos_Verduras=ft.Slider(min=0, max=5000,divisions=5000, label="Gramos{value}")
-    page.add(slider_Kilos_Verduras)    
-
+    page.add(
+        ft.Row(
+            [ft.IconButton(ft.icons.REMOVE, on_click=minus_click),caja_número_1,
+            ft.IconButton(ft.icons.ADD, on_click=plus_click)],
+            alignment=ft.MainAxisAlignment.CENTER))
+    
+    
     texto_Título_Sección_Pescados=ft.Text(value="sección de Pescados", color="black", size=20)
     page.add(texto_Título_Sección_Pescados)
     dropDownMenúPescados = ft.Dropdown(width=400, options=[ft.dropdown.Option("Almejas 20$/kg"),
@@ -59,8 +86,14 @@ def main(page: ft.Page):
                                                     ft.dropdown.Option("Atún Rojo 100.000.$/kg")])
     fila2 = ft.Row(spacing=0, controls=[dropDownMenúPescados,botón_Añadir_Pescados] )                                             
     page.add(fila2)
-    slider_Kilos_Pescados=ft.Slider(min=0, max=5000,divisions=5000, label="Gramos{value}")
-    page.add(slider_Kilos_Pescados)
+    page.add(
+        ft.Row(
+            [ft.IconButton(ft.icons.REMOVE, on_click=minus_click),caja_número_2,
+            ft.IconButton(ft.icons.ADD, on_click=plus_click)],
+            alignment=ft.MainAxisAlignment.CENTER))
+    
+
+
     
 
 
@@ -74,25 +107,15 @@ def main(page: ft.Page):
                                                     ft.dropdown.Option("Carne de Kobe 1000.000$/kg")])
     fila3 = ft.Row(spacing=0, controls=[dropDownMenúCarnes,botón_Añadir_Carnes] )                                             
     page.add(fila3)
-    slider_Kilos_Carnes=ft.Slider(min=0, max=5000,divisions=5000, label="Gramos{value}")
-    page.add(slider_Kilos_Carnes)
+    page.add(
+        ft.Row(
+            [ft.IconButton(ft.icons.REMOVE, on_click=minus_click),caja_número_3,
+            ft.IconButton(ft.icons.ADD, on_click=plus_click)],
+            alignment=ft.MainAxisAlignment.CENTER))
     
+ 
 
-
-    texto_Título_Sección_Sales=ft.Text(value="sección de Sales", color="black", size=20)
-    page.add(texto_Título_Sección_Sales)
-    dropDownMenúSales = ft.Dropdown(width=400, options=[ft.dropdown.Option("Sal marina 1$/kg"),
-                                                    ft.dropdown.Option("Sal yodada 1$/kg"),
-                                                    ft.dropdown.Option("Sal gruesa o para hornear 1$/kg"),
-                                                    ft.dropdown.Option("Sal del Himalaya o sal rosa 10$/kg"),
-                                                    ft.dropdown.Option("Sal negra de Hawái 20$/kg"),
-                                                    ft.dropdown.Option("Sal ahumada 30$/kg")])
-    fila4 = ft.Row(spacing=0, controls=[dropDownMenúSales,botón_Añadir_Sales] )                                             
-    page.add(fila4)
-    slider_Kilos_Sales=ft.Slider(min=0, max=5000,divisions=5000, label="Gramos{value}")
-    page.add(slider_Kilos_Sales)
-
-
+   
     texto_Título_Sección_Cervezas=ft.Text(value="sección de Cervezas", color="black", size=20)
     page.add(texto_Título_Sección_Cervezas)
     dropDownMenúCervezas = ft.Dropdown(width=400, options=[ft.dropdown.Option("Mahou 1$/unidad"),
@@ -101,10 +124,15 @@ def main(page: ft.Page):
                                                     ft.dropdown.Option("Alhambra 10$/unidad"),
                                                     ft.dropdown.Option("Salto Stout 20$/unidad"),
                                                     ft.dropdown.Option("Heineken 30$/unidad")])
-    fila5 = ft.Row(spacing=0, controls=[dropDownMenúCervezas,botón_Añadir_Cervezas] )                                             
-    page.add(fila5)
-    slider_Cervezas=ft.Slider(min=0, max=50,divisions=50, label="Unidades{value}")
-    page.add(slider_Cervezas)
+    fila4 = ft.Row(spacing=0, controls=[dropDownMenúCervezas,botón_Añadir_Cervezas] )                                             
+    page.add(fila4)
+    page.add(
+        ft.Row(
+            [ft.IconButton(ft.icons.REMOVE, on_click=minus_click),caja_número_4,
+            ft.IconButton(ft.icons.ADD, on_click=plus_click)],
+            alignment=ft.MainAxisAlignment.CENTER))
+    
+   
     
 
     texto_Título_Sección_Botellas=ft.Text(value="sección de Botellas", color="black", size=20)
@@ -115,16 +143,26 @@ def main(page: ft.Page):
                                                     ft.dropdown.Option("champagne Francés 80$/unidad"),
                                                     ft.dropdown.Option("Jageer 20$/unidad"),
                                                     ft.dropdown.Option("Tequila 30$/unidad")])
-    fila6 = ft.Row(spacing=0, controls=[dropDownMenúBotellas,botón_Añadir_Botellas] )                                             
+    fila5 = ft.Row(spacing=0, controls=[dropDownMenúBotellas,botón_Añadir_Botellas] )                                             
+    page.add(fila5)
+    page.add(
+        ft.Row(
+            [ft.IconButton(ft.icons.REMOVE, on_click=minus_click),caja_número_5,
+            ft.IconButton(ft.icons.ADD, on_click=plus_click)],
+            alignment=ft.MainAxisAlignment.CENTER))
+
+
+    #slider_Botellas=ft.Slider(min=0, max=50,divisions=50, label="Unidades{value}")
+    #page.add(slider_Botellas)
+
+
+
+    fila6 = ft.Row(spacing=250, controls=[botón_Finalizar_Compra] )                                             
     page.add(fila6)
-    slider_Botellas=ft.Slider(min=0, max=50,divisions=50, label="Unidades{value}")
-    page.add(slider_Botellas)
-
-
-
-    fila7 = ft.Row(spacing=250, controls=[botón_Finalizar_Compra] )                                             
-    page.add(fila7)
         
+     
+
+    
 
 
 
